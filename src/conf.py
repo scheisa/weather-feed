cf163070b4eadbbffb64d64210178bdb437fc607
scheisa <starostindenis710@gmail.com>
2023-08-18 10:38:38 +0300

    initial commit

---
+++ src/conf.py
@@ -0,0 +1,79 @@
+api_key = "ab9e7b341c1a4143b03175931230908"
+url = "http://api.weatherapi.com/v1/forecast.json?key=ab9e7b341c1a4143b03175931230908&q=Konotop&days=1&aqi=yes&alerts=yes"
+
+
+def define_data(data):
+    global last_updated
+    last_updated = data["current"]["last_updated"]
+
+    # location data
+    global city_name
+    city_name = data["location"]["name"]
+    global region_name
+    region_name = data["location"]["region"]
+    global country_name
+    country_name = data["location"]["country"]
+
+    # forecast data
+    global date
+    date = data["forecast"]["forecastday"][0]["date"]
+    global maxtemp_c
+    maxtemp_c = data["forecast"]["forecastday"][0]["day"]["maxtemp_c"]
+    global mintemp_c
+    mintemp_c = data["forecast"]["forecastday"][0]["day"]["mintemp_c"]
+    global avgtemp_c
+    avgtemp_c = data["forecast"]["forecastday"][0]["day"]["avgtemp_c"]
+    global maxwind_kph
+    maxwind_kph = data["forecast"]["forecastday"][0]["day"]["maxwind_kph"]
+
+    # chances
+    global will_it_rain
+    will_it_rain = data["forecast"]["forecastday"][0]["day"]["daily_will_it_rain"]
+    global chance_of_rain
+    chance_of_rain = data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]
+    global will_it_snow
+    will_it_snow = data["forecast"]["forecastday"][0]["day"]["daily_will_it_snow"]
+    global chance_of_snow
+    chance_of_snow = data["forecast"]["forecastday"][0]["day"]["daily_chance_of_snow"]
+
+    # weather
+    global weather_avg
+    weather_avg = data["forecast"]["forecastday"][0]["day"]["condition"]["text"]
+    global weather_avg_icon
+    weather_avg_icon = data["forecast"]["forecastday"][0]["day"]["condition"]["icon"].replace("//", "")
+
+    # astro
+    global sunrise
+    sunrise = data["forecast"]["forecastday"][0]["astro"]["sunrise"]
+    global sunset
+    sunset = data["forecast"]["forecastday"][0]["astro"]["sunset"]
+    global moonrise
+    moonrise = data["forecast"]["forecastday"][0]["astro"]["moonrise"]
+    global moonset
+    moonset = data["forecast"]["forecastday"][0]["astro"]["moonset"]
+    global moon_phase
+    moon_phase = data["forecast"]["forecastday"][0]["astro"]["moon_phase"]
+
+    global hour
+    hour = data["forecast"]["forecastday"][0]["hour"]
+
+    global main_hours
+    main_hours = [hour[i] for i in range(0, len(hour), 3)]
+    main_hours.append(hour[-1])
+
+    global hour_data
+    hour_data = {}
+    for i in range(len(main_hours)):
+        hour_data[f"{main_hours[i]['time']}"] = {
+            "time": main_hours[i]["time"],
+            "temp_c": main_hours[i]["temp_c"],
+            "feels_like": main_hours[i]["feelslike_c"],
+            "desc": main_hours[i]["condition"]["text"],
+            "desc_icon": main_hours[i]["condition"]["icon"].replace("//", ""),
+            "wind_kph": main_hours[i]["wind_kph"],
+            "wind_dir": main_hours[i]["wind_dir"],
+            "will_it_rain": main_hours[i]["will_it_rain"],
+            "chance_of_rain": main_hours[i]["chance_of_rain"],
+            "will_it_snow": main_hours[i]["will_it_snow"],
+            "chance_of_snow": main_hours[i]["chance_of_snow"],
+        }
---
+++ src/main.py
@@ -0,0 +1,49 @@
+import conf
+import requests
+
+  #<item>
+    #<title>RSS Tutorial</title>
+    #<link>https://www.w3schools.com/xml/xml_rss.asp</link>
+    #<description>New RSS tutorial on W3Schools</description>
+    #<content>New RSS tutorial on W3Schools bla bla bla</content>
+  #</item>
+
+def gen_item():
+    global file_content
+    with open("sample.xml", "r+") as file:
+        file_content = [line.replace("\n", "") for line in file if line != "\n"]
+
+    item_title = f"<title>Weather forecast for {conf.date}</title>"
+    item_link = f"<link>{0}</link>"
+    item_description = f"<description><![CDATA[\
+            <img src='{conf.weather_avg_icon}'>\
+            ]> </description>"
+
+    item_content = f"<content>Weather forecast for {conf.date} in {conf.city_name}({conf.country_name}, {conf.region_name})\n\
+            General info:\n\
+            Date: {conf.date}\n\
+            {conf.weather_avg}\n\
+            Max tempreture: {conf.maxtemp_c}\n\
+            Min tempreture: {conf.mintemp_c}\n\
+            Avarege tempreture {conf.avgtemp_c}\n\
+            Average wind speed: {conf.maxwind_kph}\n\
+            </content>"
+    print(item_title)
+    print(item_description)
+    print(item_content)
+
+def main(response):
+    data = response.json()
+    conf.define_data(data)
+
+    # print(conf.hour_data)
+    # print(list(conf.hour_data.values())[0])
+    gen_item()
+    print(file_content)
+
+response = requests.get(conf.url)
+
+if response.status_code == 200:
+    main(response)
+else:
+    print("FAILED")
